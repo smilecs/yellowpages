d7628333bce90ae2ed7b2bf4562772cbeb4df24a
smilecs <mumene@gmail.com>
2016-08-12 18:31:43 +0100

    manually removing d item since d issue only occurs on server

--- assets/js/unapproved.js
+++ assets/js/unapproved.js
@@ -1,9 +1,13 @@
 angular.module('yellowpages').controller('unviewCtrl', ['$scope', '$http', 'Notification', function($scope, $http, Notification){
 $scope.result = {};
+$scope.show = "hide";
+$scope.but = false;
   $http.get('/api/unapproved').success(function(data, status){
     $scope.result = data;
   });
   $scope.approve = function(pos, data){
+    $scope.but = true;
+    $scope.show = "show";
     console.log("clicked");
     console.log(data);
     //$scope.result = $scope.result.splice(pos, 1);
@@ -12,6 +16,8 @@ $scope.result = {};
     $http.post('/api/approve?q='+data).success(function(data,status){
     //$scope.result = data;
     //console.log(data);
+    $scope.show = "hide";
+    $scope.but = false;
     console.log($scope.result.splice(pos, 1));
       Notification({message: 'Listing Approved', title: 'Listing Management'});
     /*$http.get('/api/unapproved').success(function(data, status){
