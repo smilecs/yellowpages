8bc6d6cc7d821fd5f990ef07b71a587018405596
Anthony Alaribe <anthonyalaribe@gmail.com>
2017-03-29 17:58:08 +0100

    godeps save

--- web/middlewares.go
+++ web/middlewares.go
@@ -1,13 +1,10 @@
 package web
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
-	"reflect"
 	"time"
 
-	"github.com/gorilla/context"
 	"github.com/tonyalaribe/yellowpages/messages"
 )
 
@@ -37,56 +34,4 @@ func LoggingHandler(next http.Handler) http.Handler {
 	}
 
 	return http.HandlerFunc(fn)
-}
-
-func AcceptHandler(next http.Handler) http.Handler {
-	fn := func(w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get("Accept") != "application/json" {
-			messages.WriteError(w, messages.ErrNotAcceptable)
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	}
-
-	return http.HandlerFunc(fn)
-}
-
-func ContentTypeHandler(next http.Handler) http.Handler {
-	fn := func(w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get("Content-Type") != "application/json" {
-			messages.WriteError(w, messages.ErrUnsupportedMediaType)
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	}
-
-	return http.HandlerFunc(fn)
-}
-
-func BodyHandler(v interface{}) func(http.Handler) http.Handler {
-	t := reflect.TypeOf(v)
-
-	m := func(next http.Handler) http.Handler {
-		fn := func(w http.ResponseWriter, r *http.Request) {
-			val := reflect.New(t).Interface()
-			err := json.NewDecoder(r.Body).Decode(val)
-
-			if err != nil {
-				log.Println(err)
-				messages.WriteError(w, messages.ErrBadRequest)
-				return
-			}
-
-			if next != nil {
-				context.Set(r, "body", val)
-				next.ServeHTTP(w, r)
-			}
-		}
-
-		return http.HandlerFunc(fn)
-	}
-
-	return m
 }
