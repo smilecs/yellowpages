2370d6183a1f5925b698589db88ea5160db0385a
Anthony Alaribe <anthonyalaribe@gmail.com>
2017-06-01 10:18:49 +0100

    fixed the redirect to dashboard and the ssl google maps embed issue

--- config/config.go
+++ config/config.go
@@ -56,6 +56,7 @@ func CreateBleveIndex() (bleve.Index, error) {
 	// listingsMapping.AddFieldMappingsAt("specialisation", specialisationFieldMapping)
 	//
 	// mapping.AddDocumentMapping("listing", listingsMapping)
+
 	log.Println(config.BleveFile)
 	bIndex, err := bleve.New(config.BleveFile, mapping)
 	if err != nil {
